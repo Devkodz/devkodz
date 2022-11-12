539f61d8fea86198e73898bcd393bbea2ef38663
Devkodz <114940184+Devkodz@users.noreply.github.com>
2022-11-12 16:01:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<?php  include 'header.php'; ?>
+<?php  include 'loader.php'; ?>
+<div class="home-page">
+	<div class="landing">
+		<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+		  <ol class="carousel-indicators">
+		    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+		    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+		    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+		  </ol>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <div class="landing-slide">
+		      	<div class="landing-slide-1">
+		      		<h4><span class="sld-clr">we</span>'re</h4>
+		      		<h3>Fron<span class="sld-clr">tend</span><br> <span class="sld-clr">devel</span>opers</h3>
+		      		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+		      		<div class="cont-btn-1">
+		      			<div class="cont-btn-2"><a href="#">learn more</a></div>
+		      			<div class="cont-btn-3"><a href="#">buy now</a></div>
+		      		</div>
+		      	</div>
+		      </div>
+		    </div>
+
+		    <div class="carousel-item">
+		      <div class="landing-slide">
+		      	<div class="landing-slide-1">
+		      		<h4><span class="sld-clr">we</span>'re</h4>
+		      		<h3>crea<span class="sld-clr">tive</span><br> <span class="sld-clr">Desi</span>ngers</h3>
+		      		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+		      		<div class="cont-btn-1">
+		      			<div class="cont-btn-2"><a href="#">learn more</a></div>
+		      			<div class="cont-btn-3"><a href="#">buy now</a></div>
+		      		</div>
+		      	</div>
+		      </div>
+		    </div>
+
+		    <div class="carousel-item">
+		      <div class="landing-slide">
+		      	<div class="landing-slide-1">
+		      		<h4><span class="sld-clr">we</span>'re</h4>
+		      		<h3>se<span class="sld-clr">o</span><br> <span class="sld-clr">anal</span>yist</h3>
+		      		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+		      		<div class="cont-btn-1">
+		      			<div class="cont-btn-2"><a href="#">learn more</a></div>
+		      			<div class="cont-btn-3"><a href="#">buy now</a></div>
+		      		</div>
+		      	</div>
+		      </div>
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"><i class="fa fa-long-arrow-left"></i></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"><i class="fa fa-long-arrow-right"></i></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+
+	</div>
+
+	<section class="second-sec">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="second-sec-01">
+						<h3><span class="sld-clr">w</span>ho<br>a<span class="sld-clr">r</span>e<br>w<span class="sld-clr">e</span></h3>
+					</div>
+				</div>
+
+				<div class="col-md-6">
+					<div class="second-sec-02">
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry, but also the leap into electronic typesetting.</p>
+						<a href="#">
+							<span class="cont-btn-3">
+							learn more
+							</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="second-thr">
+		<div class="second-thr-01">
+			<div class="second-thr-02">
+				<h3>why <span class="sld-clr">us</span></h3>
+				<hr>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation.</p>
+			</div>
+			<div class="second-sec-03">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="second-sec-mini">
+								<div class="second-sec-mini-img">
+									<img src="img/sec-thr-02.png" class="dark01z">
+									<img src="img/sec-thr-light-02.png" class="light01z">
+								</div>
+								<hr>
+								<h4>keep pulse coding</h4>
+								<p>
+								 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								 tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="second-sec-mini">
+								<div class="second-sec-mini-img">
+									<img src="img/sec-thr-03.png" class="dark01z">
+									<img src="img/sec-thr-light-03.png" class="light01z">
+								</div>
+								<hr>
+								<h4>pass the limits</h4>
+								<p>
+								 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								 tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="second-sec-mini">
+								<div class="second-sec-mini-img">
+									<img src="img/sec-thr-01.png" class="dark01z">
+									<img src="img/sec-thr-light-01.png" class="light01z">
+								</div>
+								<hr>
+								<h4>great ideas</h4>
+								<p>
+								 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+								 tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	
+</div>
+
+<?php include 'footer.php';?>
